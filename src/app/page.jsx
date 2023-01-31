b0c381be8e89a983c3c5f80d672b322a82eec99e
Shivam Agarwal <itisshivam22@gmail.com>
2023-01-31 08:04:03 +0530

    feat: add a footer component to the about page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,15 @@
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from './page.module.css'
+import Footer from '@/components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-return   <main>
-    <h1 className='text-3xl font-bold' >Hello World</h1>
-  </main>
+  return (
+    <div>
+    <Footer />
+    </div>  
+  )
 
 };
---
+++ src/components/Footer.jsx
@@ -0,0 +1,59 @@
+import Image from 'next/image'
+import qr from '../assets/QR_code_for_mobile_English_Wikipedia.svg.webp'
+export default function Navbar() {
+    return (
+
+        <footer className="w-full bg-myGreen fixed bottom-0">
+            <div className="container mx-auto grid grid-cols-1 gap-1 px-6 py-8  md:grid-cols-4 md:gap-5">
+                <div>
+                    <h2 className="mb-6 text-xl font-myText">Our Partners</h2>
+                    <ul className="text-m hidden md:block">
+                        <li className="mb-4">
+                            <a className=" hover:underline">BAHMNI</a>
+                        </li>
+
+                    </ul>
+                </div>
+                <div>
+                    <h2 className="mb-6 text-xl font-myText">Important Links</h2>
+                    <ul className="text-m hidden md:block">
+                        <li className="mb-4">
+                            <a className=" hover:underline">Health Facility Registery</a>
+                        </li>
+
+                        <li className="mb-4">
+                            <a className=" hover:underline">Health Professionals Registery</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div>
+                    <h2 className="mb-6 text-xl font-myText">Policies</h2>
+                    <ul className="text-m hidden md:block">
+                        <li className="mb-4">
+                            <a className=" hover:underline">Data Privacyy</a>
+
+                        </li>
+                        <li className='mb-4'>
+                            <a className=" hover:underline">Policy Terms and Conditions</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div>
+                    <h2 className="mb-6 text-xl font-myText">Patient Locker</h2>
+                    <ul className="textß-m hidden md:block">
+                        <li className="mb-4">
+                            <a className=" hover:underline">
+                                <Image src={qr} alt='' className='h-12 w-12'></Image>
+                            </a>
+                        </li>
+
+                    </ul>
+                </div>
+
+            </div>
+
+        </footer>
+    )
+};
