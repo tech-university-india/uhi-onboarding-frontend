107f0d323c905bbe70d0927667b2d48870e008b2
Shivam Agarwal <itisshivam22@gmail.com>
2023-01-31 11:05:53 +0530

    feat: add the information to the about page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,6 +4,7 @@ import styles from './page.module.css'
 import Footer from '@/components/Footer.jsx'
 import Navbar from '@/components/Navbar.jsx'
 import Header from '@/components/header.jsx'
+import Info from '@/components/Info'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,6 +14,7 @@ export default function Home() {
       <div className='absolute'>
           <Navbar />
           <Header />
+          <Info />
           <Footer />
       </div>
     </>
