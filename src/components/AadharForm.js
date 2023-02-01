1e2f35dca9a6ba478ddd44b9dadeba5b742eb879
Kushagraved <kushagraved@gmail.com>
2023-02-01 10:56:42 +0530

    feat: add conditional rendering for otp

--- src/components/AadharForm.js
+++ src/components/AadharForm.js
@@ -3,24 +3,29 @@ import Image from 'next/image';
 import { useState } from "react";
 import { useRouter } from "next/router";
 import Link from 'next/link';
+import RegistrationForm from './RegistrationForm';
+
 function AadharForm(props) {
 
-  const [input, setInput] = useState({aadharInput: ''});
+  const [input, setInput] = useState({ aadharInput: '' });
+  const [valid, setValid] = useState(false);
+
   const router = useRouter();
   const handleSubmit = (event) => {
     event.preventDefault();
-   
+
     // advanced aadhar validation regex with optional space
     const validationRegex = /^\d{4}\s?\d{4}\s?\d{4}$/;
-    
+
     if (!validationRegex.test(input)) {
       alert('Please enter a valid Aadhar Number');
       router.reload(window.location.pathname)
     }
-    else{
-      window.location.href='/onboarding/user/otp'
+    else {
+      // window.location.href='/onboarding/user/otp'
+      setValid(true);
     }
-    
+
     console.log(input);
   }
 
@@ -28,14 +33,38 @@ function AadharForm(props) {
     <div class="items-center h-screen w-full">
       <div class="w-full bg-white rounded shadow-xl p-12 m-8 md:max-w-xl md:mx-auto">
         <form class="mb-4" method="GET">
-        <Image src={logo} alt="UHI Logo" class='w-max'/>
+          <Image src={logo} alt="UHI Logo" class='w-max' />
           <div class="mb-6 ">
             <label for="aadharNum" class="block text-xs mb-1">Aadhar Number</label>
-            <input onChange={(e) => setInput(e.target.value) } class="w-full border rounded p-2 outline-none focus:shadow-outline" type="aadharInput" name="aadharInput" id="aadharInput" placeholder="Aadhar Number"></input>
+            <input onChange={(e) => setInput(e.target.value)} class="w-full border rounded p-2 outline-none focus:shadow-outline" type="aadharInput" name="aadharInput" id="aadharInput" placeholder="Aadhar Number"></input>
           </div>
-          <div class='flex justify-evenly w-full'>
-            <button class="bg-black hover:bg-green-700 text-white  text-lg font-semibold px-4 py-2 rounded">Back</button>
-            <Link href='/onboarding/user/otp'><button className="bg-green-700 hover:bg-black text-white  text-lg font-semibold px-4 py-2 rounded" onClick={handleSubmit} > Send OTP</button></Link>
+          <div className={`justify-evenly w-full ${valid ? '' : 'flex'}`}>
+
+            {
+              !valid && <button class="bg-black hover:bg-green-700 text-white  text-lg font-semibold px-4 py-2 rounded">Back</button>
+            }
+            {
+              valid && <>
+
+                <div class="mb-6 ">
+                  <label for="OTP" class="block text-xs mb-1">Enter OTP</label>
+                  <input class="w-full border rounded p-2 outline-none focus:shadow-outline" type="OTPInput" name="OTPInput" id="OTPInput" placeholder="Enter OTP"></input>
+                </div>
+                <div class="mb-6 ">
+                  <label for="OTP" class="w-full block text-xs mb-1">Enter Mobile Number</label>
+                  <input class="w-full border rounded p-2 outline-none focus:shadow-outline" type="mobileInput" name="mobileInput" id="mobileInput" placeholder="Enter Mobile Number"></input>
+                </div>
+                <div class='flex justify-evenly w-full'>
+                  <button class="bg-black hover:bg-green-700 text-white  text-lg font-semibold px-4 py-2 rounded">Back</button>
+                  <button class="bg-green-700 hover:bg-black text-white  text-lg font-semibold px-4 py-2 rounded">Submit</button>
+                </div>
+
+
+              </>
+            }
+            {
+              !valid && <Link href='/onboarding/user/otp'><button className="bg-green-700 hover:bg-black text-white  text-lg font-semibold px-4 py-2 rounded" onClick={handleSubmit} > Send OTP</button></Link>
+            }
           </div>
         </form>
       </div>
