91e2bba881aad928a0c3460ca879221e4d80796e
SaatwikRishi <saatwik_s@mckinsey.com>
2023-02-08 21:51:06 +0530

    opt: Made optimizations in code

--- src/pages/users/login/index.js
+++ src/pages/users/login/index.js
@@ -15,12 +15,10 @@ export default function UserLogin () {
         }} name="loginType" className='mr-3' /><label htmlFor={type}>{label}</label>
     </div>
 
-  const loginWithOTP = (label) => (<div>
-        <br />
-        <p className='font-bold text-xl'>Login with OTP</p>
-        <br />
+  const loginWithOTP = (label) => (<>
+        <p className='font-bold text-xl my-6 pl-1'>Login with OTP</p>
         <CustomInput label={label} placeholder={'Enter OTP'} />
-    </div>)
+    </>)
 
   const handleAbhaNumberOnChange = (event) => {
     const input = event.target.value
@@ -39,46 +37,38 @@ export default function UserLogin () {
         {/* <div></div> Header */}
         <div className="justify-center flex">
             <FormBox >
-                <div>
-                    <CustomInput label={'ABHA Number'} placeholder={'Enter ABHA Number'} onChange={handleAbhaNumberOnChange}></CustomInput>
-                    <br />
-                    <br />
-                    <p className='font-bold text-xl'>Login Using:</p>
-                    <br />
-                    <div>
-                    </div>
-                    {getLoginTypeRadioBox('Personal Details', LOGIN_TYPE.PERSONAL_DETAILS)}
-                    {getLoginTypeRadioBox('OTP via Aadhar Number', LOGIN_TYPE.OTP_VIA_AADHAR)}
-                    {getLoginTypeRadioBox('OTP via Mobile Number ( Aadhaar not linked )', LOGIN_TYPE.OTP_VIA_MOBILE)}
+                <CustomInput label={'ABHA Number'} placeholder={'Enter ABHA Number'} onChange={handleAbhaNumberOnChange}></CustomInput>
+                <p className='font-bold text-xl mt-5 mb-3'>Login Using:</p>
+                {getLoginTypeRadioBox('Personal Details', LOGIN_TYPE.PERSONAL_DETAILS)}
+                {getLoginTypeRadioBox('OTP via Aadhar Number', LOGIN_TYPE.OTP_VIA_AADHAR)}
+                {getLoginTypeRadioBox('OTP via Mobile Number ( Aadhaar not linked )', LOGIN_TYPE.OTP_VIA_MOBILE)}
 
-                    {currentLoginType && <> <div>
-                        {currentLoginType === LOGIN_TYPE.OTP_VIA_AADHAR && loginWithOTP('Enter OTP ( OTP sent to aadhaar linked mobile number )')}
-                    </div>
-                        <div>
-                            {currentLoginType === LOGIN_TYPE.OTP_VIA_MOBILE && loginWithOTP('Enter OTP ( OTP sent to ABHA linked mobile number )')}
-
-                        </div>
-                        {currentLoginType === LOGIN_TYPE.PERSONAL_DETAILS && <div>
-                            <br />
-                            <p className='font-bold text-xl'>Login with Personal Details</p>
-                            <br />
-                            <CustomInput label={'Name'} placeholder={'Enter the name'} />
-                            <br />
-                            <div className='flex justify-between'>
-                                <div className='flex-row'>
-                                    <div className='flex'>
-                                        <label htmlFor='gender'>Gender</label> <div className="text-red-500 font-medium text-2xl">&nbsp;*</div>
-                                    </div>
-                                    <select className='w-full border rounded-md px-5 pt-4 pb-5 outline-none focus:shadow-outline' name='gender' id='gender'>
-                                        {['Male', 'Female'].map(element => <option key={Math.random()} value={element}>{element}</option>)}
-                                    </select>
-                                </div>
-                                <CustomInput label={'Year of Birth'} placeholder={'Enter Year'} type={'number'} />
-                            </div>
-                        </div>}</>}
-
-                    {/* TODO: ADD BUTTONS */}
+                {currentLoginType && <> <div>
+                    {currentLoginType === LOGIN_TYPE.OTP_VIA_AADHAR && loginWithOTP('Enter OTP ( OTP sent to aadhaar linked mobile number )')}
                 </div>
+                    <div>
+                        {currentLoginType === LOGIN_TYPE.OTP_VIA_MOBILE && loginWithOTP('Enter OTP ( OTP sent to ABHA linked mobile number )')}
+
+                    </div>
+                    {currentLoginType === LOGIN_TYPE.PERSONAL_DETAILS && <div>
+                        <p className='font-bold text-xl mt-8 mb-5'>Login with Personal Details</p>
+                        <CustomInput label={'Name'} placeholder={'Enter the name'} />
+                        <br />
+                        <div className='flex justify-between'>
+                            <div className='flex-row'>
+                                <div className='flex'>
+                                    <label htmlFor='gender'>Gender</label> <div className="text-red-500 font-medium text-2xl">&nbsp;*</div>
+                                </div>
+                                <select className='w-full border rounded-md px-5 pt-4 pb-5 outline-none focus:shadow-outline' name='gender' id='gender'>
+                                    {['Male', 'Female'].map(element => <option key={Math.random()} value={element}>{element}</option>)}
+                                </select>
+                            </div>
+                            <CustomInput label={'Year of Birth'} placeholder={'Enter Year'} type={'number'} />
+                        </div>
+                    </div>}</>}
+
+                {/* TODO: ADD BUTTONS */}
+
             </FormBox>
 
         </div>
