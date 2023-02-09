c4cb67d1ff821dc6878a0bfecfa9390514a4fbb3
Shivam Agarwal <itisshivam22@gmail.com>
2023-02-09 16:37:50 +0530

    fix: change class to className property in Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,22 +7,22 @@ import Dropdown from "./Dropdown";
 
 export default function NavigationBar() {
     return (
-        <nav class="w-screen bg-uhigreen top-0 relative">
-            <div class='container mx-auto py-1 flex font-sans justify-between items-center'>
-                <div class=''>
+        <nav className="w-screen bg-uhigreen top-0 relative">
+            <div className='container mx-auto py-1 flex font-sans justify-between items-center'>
+                <div className=''>
                     <Image src={logo} alt='' class='h-24 w-24 rounded-full ml-0'></Image>
                 </div>
-                <ul class="hidden md:flex space-x-10 text-xl font-myText">
-                    <li class='hover:font-bold hover:bg-green-500 hover:rounded-full text-2xl p-2'>Home</li>
+                <ul className="hidden md:flex space-x-10 text-xl font-myText">
+                    <li className='hover:font-bold hover:bg-green-500 hover:rounded-full text-2xl p-2'>Home</li>
                     <li>
                         <Dropdown />
                     </li>
-                    <li class='hover:font-bold hover:bg-green-500 hover:rounded-full text-2xl p-2'>Documentation</li>
-                    <li class='hover:font-bold hover:bg-green-500 hover:rounded-full text-2xl p-2'>FAQs</li>
-                    <li class='hover:font-bold hover:bg-green-500 hover:rounded-full text-2xl p-2'>Contact Us</li>
-                    <li class='hover:font-bold hover:bg-green-500 hover:rounded-full text-2xl p-2'>Login</li>
+                    <li className='hover:font-bold hover:bg-green-500 hover:rounded-full text-2xl p-2'>Documentation</li>
+                    <li className='hover:font-bold hover:bg-green-500 hover:rounded-full text-2xl p-2'>FAQs</li>
+                    <li className='hover:font-bold hover:bg-green-500 hover:rounded-full text-2xl p-2'>Contact Us</li>
+                    <li className='hover:font-bold hover:bg-green-500 hover:rounded-full text-2xl p-2'>Login</li>
                 </ul>
-                <Image src={hamburger} alt='' class='w-10 h-10 rounded-full md:hidden cur'></Image>
+                <Image src={hamburger} alt='' className='w-10 h-10 rounded-full md:hidden cur'></Image>
             </div>
         </nav>
     )
