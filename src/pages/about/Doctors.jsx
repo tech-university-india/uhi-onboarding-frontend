eae577f7172142910ec436b12ffd5360fd66e84b
Shivam Agarwal <itisshivam22@gmail.com>
2023-02-13 21:33:35 +0530

    fix: combine multiple components into one

--- src/pages/about/Doctors.jsx
+++ src/pages/about/Doctors.jsx
@@ -1,6 +1,5 @@
 import "../../app/globals.css";
 import Footer from '@/components/Footer.jsx'
-import Navbar from '@/components/Navbar.jsx'
 import Header from "@/components/Header";
 import Info from "@/components/Info";
 import screenText from "screenText";
@@ -9,8 +8,7 @@ import DoctorImage from '@/assets/citizens_doctors_info_diagram.png'
 export default function Doctors() {
   return (
     <>
-      <div className='absolute'>
-          <Navbar />
+      <div>
           <Header header={screenText.about.doctors.header} />
           <Info info={screenText.about.doctors.info} imagePath={DoctorImage} />
           <Footer />
--- src/pages/onboarding/user/otp/index.js
+++ src/pages/onboarding/user/otp/index.js
@@ -1,10 +1,8 @@
 import Footer from "@/components/Footer";
-import NavigationBar from "@/components/NavigationBar";
 import RegistrationForm from "@/components/RegistrationForm";
 
 export default function OTPRegistration() {
     return (<div>
-        <NavigationBar />
             <RegistrationForm />
             <Footer />
     </div>
