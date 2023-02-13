47af11f2e5eb94e43f768fd55bce786f28a009b3
SaatwikRishi <saatwik_s@mckinsey.com>
2023-02-13 16:32:51 +0530

    modify page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Info from "@/components/Info";
 import NavBar from "@/components/Navbar";
+import screenText from "screenText";
 
 export default function Index() {return  <div className='absolute'>
 <NavBar />
