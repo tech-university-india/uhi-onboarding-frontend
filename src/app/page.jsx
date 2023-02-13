4c39c7609abff95888971e8c653aef9267f17399
SaatwikRishi <saatwik_s@mckinsey.com>
2023-02-13 16:01:35 +0530

    chore: Modify Navbar path

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,7 @@
 import { Inter } from '@next/font/google'
 import Footer from '@/components/Footer.jsx'
 import Navbar from '@/components/Navbar.jsx'
-import Header from '@/components/header.jsx'
+import Header from '@/components/Header.jsx'
 import Info from '@/components/Info'
 import screenText from 'screenText'
 import UHIImage from '@/assets/uhi_diagram.png'
--- src/pages/onboarding/user/index.js
+++ src/pages/onboarding/user/index.js
@@ -1,6 +1,6 @@
 import AadharForm from "@/components/AadharForm";
-import Footer from "@/components/Footer";
-import NavBar from "@/components/NavBar";
+import Footer from "@/components/Footer.jsx";
+import NavBar from "@/components/NavBar.jsx";
 import Link from 'next/link';
 export default function User() {
     return (<div>
