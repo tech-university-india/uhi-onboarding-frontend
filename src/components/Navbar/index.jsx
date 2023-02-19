8eed563edfdc15d5573db2fed7c483c47498fe56
SaatwikRishi <saatwik_s@mckinsey.com>
2023-02-19 22:01:49 +0530

    feat: Add image for navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,14 +1,16 @@
 'use client'
-// import logo from '../../assets/ABDM_English_Logo_with_tagline_aea4f8d600.png'
+import Image from 'next/image'
+import logo from '../../assets/uhi-logo-no-bg.png'
 // import hamburger from '../../assets/Hamburger_icon.svg.png'
 import Dropdown from './Dropdown'
 import Link from 'next/link'
 import React from 'react'
 export default function NavBar () {
   return (
-    <nav className="w-screen bg-uhigreen top-0 relative">
-      <div className="container mx-auto py-1 flex font-sans justify-between items-center">
-        <ul className="hidden md:flex space-x-10 text-xl font-myText">
+    <nav className="p-2 w-screen bg-uhigreen">
+      <div className="mx-auto flex font-sans justify-between items-center">
+        <Image src={logo} className="h-[80px] w-[80px]" />
+        <ul className="hidden md:flex space-x-4 text-l font-myText">
           <Link href="/">
             <li className="hover:font-bold hover:bg-green-500 hover:rounded-full text-2xl p-2">Home</li>
           </Link>
