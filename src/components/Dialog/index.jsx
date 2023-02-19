b948fcdfa429749de51c6bb67a338a2b4524887c
SaatwikRishi <saatwik_s@mckinsey.com>
2023-02-19 21:42:27 +0530

    opt: Alert dialog

---
+++ src/components/Dialog/index.jsx
@@ -0,0 +1,10 @@
+import classNames from 'classnames'
+import React from 'react'
+
+export default function Dialog ({ isOpen, children }) {
+  return isOpen
+    ? <div className={classNames('bg-white shadow-xl bg-white-500 absolute top-0 left-0 bottom-0 right-0 m-auto max-w-sm  md:max-w-screen-md w-[fit-content] h-[fit-content] justify-center items-center p-10')} >
+      <span>{children}</span>
+    </div>
+    : <></>
+}
--- src/components/DialogBox/index.jsx
+++
@@ -1,8 +0,0 @@
-import classNames from 'classnames'
-import React from 'react'
-
-export default function Dialog ({ isOpen, children }) {
-  return <div className={classNames(`${isOpen ? '' : 'hidden'}`, 'bg-white shadow-xl bg-white-500 absolute top-0 left-0 bottom-0 right-0 m-auto max-w-sm  md:max-w-screen-md w-[fit-content] h-[fit-content] justify-center items-center p-10')} >
-    <span>{children}</span>
-  </div>
-}
