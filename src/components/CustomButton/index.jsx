5d87b8deb27531379aa2eafde6a0fcb9a0a1d60a
Saatwik-S <saatwik_s@mckinsey.com>
2023-03-03 08:25:22 +0530

    feat: create custom button

--- src/components/CustomButton/index.jsx
+++ src/components/CustomButton/index.jsx
@@ -4,12 +4,14 @@ import { ButtonColors } from '@/common/constants'
 import classNames from 'classnames'
 
 export default function CustomButton ({
+  onClick,
   children,
   color = ButtonColors.green,
   isSelected = false
 }) {
   return (
     <button
+      onClick={onClick}
       className={classNames(
         !isSelected ? 'shadow-lg' : 'shadow-inner',
         'px-6 py-3 rounded-lg text-base font-medium border-r-2'
@@ -26,5 +28,6 @@ export default function CustomButton ({
 CustomButton.propTypes = {
   children: PropTypes.node.isRequired,
   color: PropTypes.oneOf(Object.values(ButtonColors)),
-  isSelected: PropTypes.bool
+  isSelected: PropTypes.bool,
+  onClick: PropTypes.func
 }
