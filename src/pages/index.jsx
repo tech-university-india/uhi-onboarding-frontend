5fb384f3e711ea17121ef57f05287542b3c878a6
Saatwik-S <saatwik_s@mckinsey.com>
2023-03-03 08:26:54 +0530

    chore: Remove unused imports

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,15 +1,11 @@
 import React from 'react'
 
-import Info from '@/components/Info'
-import screenText from 'screenText'
-import UHIImage from '@/assets/uhi_diagram.png'
-
 // const inter = Inter({ subsets: ['latin'] })
 
 export default function Home () {
   return (
     <>
-     <h1>Hello World</h1>
+      <h1>Hello World</h1>
     </>
   )
 };
