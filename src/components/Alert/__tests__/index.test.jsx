2b33f8d9febaacf5a3bcff81c5e8a8696e33ba18
SaatwikRishi <38259107+SaatwikRishi@users.noreply.github.com>
2023-03-05 22:22:12 +0530

    Chore/testing (#22)
    * chore: rename alert.jsx to index.jsx inside the folder
    
    * chore: setup test cases and test alert component
    
    * fix: updated tests
    
    ---------
    
    Co-authored-by: SaatwikRishi <saatwik_s@mckinsey.com>

---
+++ src/components/Alert/__tests__/index.test.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable no-undef */
+import React from 'react'
+import Alert from '..'
+import { fireEvent, render } from '@testing-library/react'
+describe('Tests for Alert Dialog', () => {
+  it('should render the component', () => {
+    const wrapper = render(<Alert />)
+    expect(wrapper).toMatchSnapshot()
+  })
+  it('should render the component with props', () => {
+    const screen = render(<Alert input="test" />)
+    expect(screen.getByText('test')).toBeTruthy()
+  })
+  it('should call the close alert callback when the close button is clicked', () => {
+    const setAlert = jest.fn()
+    const screen = render(<Alert setAlert={setAlert} />)
+
+    fireEvent.click(screen.getByRole('button'))
+    expect(setAlert).toBeCalled()
+  })
+})
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 export default function Home () {
   return (
     <>
-      <p className="th-h1" >Hello World</p>
+      <h1>Hello World</h1>
     </>
   )
 };
