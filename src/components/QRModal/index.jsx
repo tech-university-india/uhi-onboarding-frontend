11779875527eff8ae89f748293221ca0bfb01956
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2023-03-05 22:37:27 +0530

    test:add tests for QR Modal component

--- src/components/QRModal/index.jsx
+++ src/components/QRModal/index.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import { qrCode } from "../../constants/qrCode";
-
 const QRModal = ({ visible, onClose }) => {
   const handleClose = (e) => {
     if (e.target.id === "container") onClose();
   };
   if (!visible) return null;
+  const qrCode = '64base-imageformat'
   return (
     <div
       id="container"
       onClick={handleClose}
+      data-testid="container"
       className="fixed inset-0 bg-black bg-opacity-10 backdrop-blur-sm flex justify-center items-center"
     >
       <div className="bg-white w-96 h-96 rounded-lg shadow-lg flex flex-col justify-center items-center">
