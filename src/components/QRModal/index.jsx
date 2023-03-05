4fc6c4d692d7b158220adfb2272abcfff6341893
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2023-03-05 18:54:51 +0530

    feat: render aadhar photo and add qrCode modal

---
+++ src/components/QRModal/index.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { qrCode } from "../../constants/qrCode";
+
+const QRModal = ({ visible, onClose }) => {
+  const handleClose = (e) => {
+    if (e.target.id === "container") onClose();
+  };
+  if (!visible) return null;
+  return (
+    <div
+      id="container"
+      onClick={handleClose}
+      className="fixed inset-0 bg-black bg-opacity-10 backdrop-blur-sm flex justify-center items-center"
+    >
+      <div className="bg-white w-96 h-96 rounded-lg shadow-lg flex flex-col justify-center items-center">
+        <div className="w-80 h-80 bg-primary rounded-lg flex justify-center items-center">
+          <img src={`data:image/jpeg;base64,${qrCode}`} alt="qr-code" className="w-64 h-64" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default QRModal;
