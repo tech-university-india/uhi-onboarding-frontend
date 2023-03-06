92b8f7a0e1ccc951a95ea3099a82da71501dc4c8
Souris Ash <suorees113@gmail.com>
2023-03-06 14:35:20 +0530

    fix: move colours to colors instead of color and add shadows

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,6 @@ module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      color: {
-        primary: '#B6EBB7',
-        secondary: '#008080'
-      },
       backgroundColor: {
         primary: '#B6EBB7',
         secondary: '#008080',
@@ -20,8 +16,14 @@ module.exports = {
         deactivate: '#FBA997',
         delete: '#FF6464'
       },
-
+      boxShadow: {
+        textBox: '0px 4px 4px rgba(0, 0, 0, 0.25)',
+        textBoxInset: 'inset 0 4px 4px rgba(0, 0, 0, 0.25)'
+      },
       colors: {
+        primary: '#B6EBB7',
+        secondary: '#008080',
+        preFillText: '#4D4242'
       },
       fontFamily: {
         sans: ['Avenir', 'sans-serif']
