f92c8086bb93911a4103e90eae2ad73086152416
Shreyas Shettigar <shreyasshettigar34@gmail.com>
2023-03-13 12:16:32 +0530

    fix:add padding for QR code container

--- src/components/QRModal/index.jsx
+++ src/components/QRModal/index.jsx
@@ -10,7 +10,7 @@ const QRModal = ({ visible, onClose }) => {
       id="container"
       onClick={handleClose}
       data-testid="container"
-      className="fixed inset-0 bg-black bg-opacity-10 backdrop-blur-sm flex justify-center items-center"
+      className="fixed inset-0 bg-black bg-opacity-10 backdrop-blur-sm flex justify-center items-center p-4"
     >
       <div className="bg-white w-96 h-96 rounded-lg shadow-lg flex flex-col justify-center items-center">
         <div className="w-80 h-80 bg-primary rounded-lg flex justify-center items-center">
