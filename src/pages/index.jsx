7c41bf4cb89a08d93ad6bd8c32aec395b04c0f51
Kushagraved <kushagraved@gmail.com>
2023-03-13 14:16:07 +0530

    Feat/otp (#26)
    * feat: add otp component
    
    * fix: otp take invalid characters
    
    * feat: add test for otp
    
    * fix: component name
    
    ---------
    
    Co-authored-by: SaatwikRishi <38259107+SaatwikRishi@users.noreply.github.com>

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,11 @@
+
 import React from 'react'
 // const inter = Inter({ subsets: ['latin'] })
 
-export default function Home () {
+export default function Home() {
   return (
-    <div>
-      <h1 className="text-hello">Hello World</h1>
-    </div>
-  )
+    <>
+      <p className="th-h1">Hello World</p>
+    </>
+  );
 }
