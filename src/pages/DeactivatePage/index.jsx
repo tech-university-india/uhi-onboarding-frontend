bd1068461697847cc00f842447edbe157fb12b93
Ishit <ishitsingh2001@gmail.com>
2023-03-13 15:11:35 +0530

    chore:update Dropdown to use SectionTitleDisplay component

---
+++ src/pages/DeactivatePage/index.jsx
@@ -0,0 +1,12 @@
+import Header from '@/components/Header'
+import React from 'react'
+
+const DeactivatePage = () => {
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default DeactivatePage
