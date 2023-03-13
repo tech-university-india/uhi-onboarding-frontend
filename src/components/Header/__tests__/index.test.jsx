348a4ef7f236d808052a372fdcfc83a39b8f2e5f
Souris Ash <suorees113@gmail.com>
2023-03-13 16:40:44 +0530

    fix: merge conflict markers fixed in header tests (#34)

--- src/components/Header/__tests__/index.test.jsx
+++ src/components/Header/__tests__/index.test.jsx
@@ -5,12 +5,7 @@ import { render } from '@testing-library/react'
 
 describe('Header', () => {
   it('should render the component with text props', () => {
-<<<<<<< HEAD
-    const headerComponent = render(<Header />).asFragment()
-    expect(headerComponent).toMatchSnapshot()
-=======
     const { getByText } = render(<Header text="Hello" />)
     expect(getByText('Hello')).toBeTruthy()
->>>>>>> 7c41bf4cb89a08d93ad6bd8c32aec395b04c0f51
   })
 })
