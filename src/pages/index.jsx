1161a7bd4889b4d9f8705e84500b8e41b46f4686
ishit-007 <86899201+ishit-007@users.noreply.github.com>
2023-03-13 13:35:35 +0530

    Feat/custom drop down (#29)
    * feat: add dropdown for displaying user info, styled for mobile screens
    
    * feat : add Dropdown component to display user info

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-
 // const inter = Inter({ subsets: ['latin'] })
 
 export default function Home () {
   return (
-    <>
+    <div>
       <h1 className="text-hello">Hello World</h1>
-    </>
+    </div>
   )
 }
