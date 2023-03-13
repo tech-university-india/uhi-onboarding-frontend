2e80f9a37227d8dbcb9aff41d25237d51710fc61
Souris Ash <suorees113@gmail.com>
2023-03-13 16:01:13 +0530

    feat: text display (#32)
    * feat: add text displays
    
    * chore: fix variable naming in tests
    
    * fix: interchange collapse and expand alt texts
    
    * feat: add section title
    
    * chore: add explanation for props
    
    * fix: restore homepage

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,10 +2,10 @@
 import React from 'react'
 // const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Home () {
   return (
     <>
       <p className="th-h1">Hello World</p>
     </>
-  );
+  )
 }
