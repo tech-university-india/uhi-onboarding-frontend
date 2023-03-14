293be68023c8b87da5ead3fee6262c17aa9857cc
Shivam Agarwal <itisshivam22@gmail.com>
2023-03-14 13:17:39 +0530

    fix: remove comments

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,9 @@
 import React from 'react'
-// const inter = Inter({ subsets: ['latin'] })
-// import MyApp from './_app'
-// import PatientDetails from './PatientDetails'
 
 export default function Home () {
   return (
     <>
       <p className="th-h1">Hello World</p>
-      {/* <MyApp Component={PatientDetails} pageProps={{}} /> */}
     </>
   )
 }
