f8f072b950c824dd9c36ea7d9a2ec405288fa9a7
Shivam Agarwal <itisshivam22@gmail.com>
2023-03-15 01:01:16 +0530

    fix: remove patient page from rendering at root url

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
-import MyApp from './_app'
-import PatientDetails from './PatientDetails'
 
 export default function Home () {
   return (
     <>
-      {/* <p className="th-h1">Hello World</p> */}
-      <MyApp Component={PatientDetails} pageProps={{}} />
+      <p className="th-h1">Hello World</p>
     </>
   )
 }
